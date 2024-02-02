cb148c844b88f0fc7523e3b710cbcf6405ae1961
Gaby191202 <ppmc1985@gmail.com>
2024-02-02 18:28:17 -0400

    Realize algunos cambios dentro de la distribucion del proyecto y agrege las paginas con los componentes creados por mi compañera Estrella, hice una que otra modificacion en el proyecto para que funcionara de una manera optima

--- ProyectoLDP4.blazor/Data/Hotel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorHotelReservations.Data
-{
-    public class Hotel
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Descripcion { get; set; }
-    }
-}
---
+++ ProyectoLDP4.blazor/Data/Models/Hotel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoLDP4.blazor.Data.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+    }
+}
